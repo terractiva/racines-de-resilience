870ef47a392bf6b72c0585a7f8a2d870ed15a710
Thomas Seignette <tseignette@pm.me>
2021-04-02 22:17:03 +0200

    v0.1

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.scss';
+</script>
+
+<style lang="scss">
+	main {
+		height: 100vh;
+		width: 100vw;
+		overflow: hidden;
+	}
+</style>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Map from '$lib/Map.svelte';
+</script>
+
+<Map></Map>
