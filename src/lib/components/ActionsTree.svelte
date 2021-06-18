518801b7707c4d59efcb352848c6d3714d75625e
Thomas Seignette <tseignette@pm.me>
2021-06-18 16:58:07 +0200

    fonctionnalité(arbre): Affichage de l'arbre ajouté

---
+++ src/lib/components/ActionsTree.svelte
@@ -0,0 +1,96 @@
+<script>
+	import { fabric } from 'fabric';
+	import { onMount } from 'svelte';
+
+	export let height;
+	export let width;
+
+	/** @type import('@types/fabric').fabric.StaticCanvas */
+	let fabricCanvas;
+	let isBackgroundError = false;
+	let isBackgroundLoading = true;
+	/** @type HTMLCanvasElement */
+	let nativeCanvas;
+	/** @type [x: number, y: number] */
+	let backgroundPadding; // Espace à rajouter de chaque côté de l'arrière-plan pour qu'il prenne toute la place disponible
+
+	$: resizeCanvas(width, height);
+
+	onMount(() => {
+		initCanvas();
+	});
+
+	function initCanvas() {
+		fabricCanvas = new fabric.StaticCanvas(nativeCanvas, { selection: false });
+		fabricCanvas.setBackgroundColor('#ffffff');
+		fabricCanvas.setBackgroundImage('arbre.jpg', (_, isError) => {
+			isBackgroundLoading = false;
+
+			if (isError) {
+				isBackgroundError = true;
+			} else {
+				resizeCanvasBackground(width, height);
+			}
+		});
+	}
+
+	// Réinitialise le zoom et le déplacement
+	function resetCanvasPosition() {
+		fabricCanvas.setViewportTransform([1, 0, 0, 1, 0, 0]);
+	}
+
+	function resizeCanvas() {
+		if (!fabricCanvas) return;
+
+		resetCanvasPosition();
+		fabricCanvas.setDimensions({ height, width });
+		resizeCanvasBackground(width, height);
+	}
+
+	function resizeCanvasBackground(width, height) {
+		const background = fabricCanvas.backgroundImage;
+
+		if (!background) return;
+
+		const backgroundRatio = background.width / background.height;
+		const canvasRatio = width / height;
+
+		if (backgroundRatio < canvasRatio) {
+			background.scaleToHeight(height);
+			backgroundPadding = [(width - background.getScaledWidth()) / 2, 0];
+		} else if (backgroundRatio > canvasRatio) {
+			background.scaleToWidth(width);
+			backgroundPadding = [0, (height - background.getScaledHeight()) / 2];
+		}
+
+		background.center();
+		fabricCanvas.renderAll();
+	}
+</script>
+
+{#if isBackgroundError}
+	<p class="text-error">Impossible de charger l'arbre</p>
+{/if}
+{#if isBackgroundLoading}
+	<p>Chargement de l'arbre...</p>
+{/if}
+
+<canvas
+	bind:this={nativeCanvas}
+	style="opacity: {isBackgroundError || isBackgroundLoading ? 0 : 1};"
+/>
+
+<style lang="scss">
+	p {
+		left: 50%;
+		margin-bottom: 0;
+		position: absolute;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	canvas {
+		display: block;
+		transition: opacity 0.5s ease;
+	}
+</style>
--- src/routes/arbre-actions.svelte
+++ src/routes/arbre-actions.svelte
@@ -1,6 +1,26 @@
 <script>
+	import ActionsTree from '$lib/components/ActionsTree.svelte';
 	import PageMeta from '$lib/components/PageMeta.svelte';
 	import { actionsTreePage } from '$lib/constants/pages';
+	import { onMount } from 'svelte';
+
+	let height;
+	let width;
+
+	onMount(() => {
+		setSize();
+	});
+
+	function setSize() {
+		const main = document.getElementsByTagName('main')[0];
+
+		height = main.clientHeight;
+		width = main.clientWidth;
+	}
 </script>
 
 <PageMeta page={actionsTreePage} />
+
+<svelte:window on:resize={setSize} />
+
+<ActionsTree {height} {width} />
