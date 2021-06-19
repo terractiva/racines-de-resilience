ecbc400b2b193a523d4548b9e124b02279590843
Thomas Seignette <tseignette@pm.me>
2021-06-19 22:39:38 +0200

    fonctionnalité(site): Page d'erreur ajoutée

---
+++ src/routes/__error.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	export function load({ status }) {
+		return {
+			props: {
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	import { homePage } from '$lib/constants/pages';
+
+	export let status;
+</script>
+
+<section class="container">
+	<h1>{status}</h1>
+
+	<p>Oups, il y a eu une erreur...</p>
+
+	<a href={homePage.path} class="button">Revenir à l'accueil</a>
+</section>
+
+<style lang="scss">
+	section {
+		text-align: center;
+
+		h1 {
+			margin-top: 0;
+		}
+	}
+</style>
