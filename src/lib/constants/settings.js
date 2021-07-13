69fd0d3e72d2f2423f48bc0e692aee8c82a9edcb
briannep <83287153+briannep@users.noreply.github.com>
2021-07-13 12:33:49 +0200

    Ajout lien formulaire rejoindre communauté

--- src/lib/constants/settings.js
+++ src/lib/constants/settings.js
@@ -1,5 +1,6 @@
 const actionFontSize = 0.004; // Taille du texte des actions en % de la hauteur de l'arrière plan
 const contributeFormLink = 'https://framaforms.org/';
+const joinLinkMail = 'https://35aed784.sibforms.com/serve/MUIEAFnSiao4NqqfVb1E6Dh3bogNQm7lZxpo94LKnEXl8tA7VajSDG2bXfpx2xRT_RY4W_ekE3GoYTNo8AeRtEWao4iDGK-8-QuHV29Y8I7keBwpZJiOG2yqwvT0O6lYqJ28VYl4ZmXSGrOVe2EGv6-KSqmU0AyZnuby6rnB9utJguUhCiKWprXrWfyVtHOy33jYeOnOw2eXnyEo/';
 const nbFilterResultsShowed = 10;
 const zoomMax = 5;
 const zoomMin = 1;
