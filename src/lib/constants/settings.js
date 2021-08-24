f8cc22fcc7081ff12aa05831d000d39c4e1a60bf
Thomas Seignette <tseignette@pm.me>
2021-08-24 16:49:58 +0200

    tâche(arbre): Zoom maximum augmenté

--- src/lib/constants/settings.js
+++ src/lib/constants/settings.js
@@ -3,7 +3,7 @@ const nbFilterResultsShowed = 10;
 const treeActionFontSize = 0.004; // Taille du texte en % de la hauteur de l'arrière plan
 const treeSubcategoryFontSize = 0.006;
 const zoomIncrement = 0.5;
-const zoomMax = 6;
+const zoomMax = 10;
 const zoomMin = 1;
 
 export {
