98307f22a6a8bc924181b63abb25bd9c5c18d57f
Thomas Seignette <tseignette@pm.me>
2021-09-21 09:07:10 +0200

    correction: Action sans source corrigée

--- src/lib/components/Action.svelte
+++ src/lib/components/Action.svelte
@@ -14,23 +14,25 @@
 		<p class="action-text">{@html action.text}</p>
 	</header>
 
-	<p class="action-sources {textClasses}">
-		<b>
-			{#each action.sources as source, index}
-				{#if source.link}<a href={source.link} rel="external" target="_blank"
-						>{source.name}<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-							><path
-								d="m14.828 12 1.415 1.414 2.828-2.828a4 4 0 0 0-5.657-5.657l-2.828 2.828L12 9.172l2.828-2.829a2 2 0 1 1 2.829 2.829L14.828 12zM12 14.829l1.414 1.414-2.828 2.828a4 4 0 0 1-5.657-5.657l2.828-2.828L9.172 12l-2.829 2.829a2 2 0 1 0 2.829 2.828L12 14.828z"
-								fill="currentColor"
-							/><path
-								d="M14.829 10.586a1 1 0 0 0-1.415-1.415l-4.242 4.243a1 1 0 1 0 1.414 1.414l4.242-4.242z"
-								fill="currentColor"
-							/></svg
-						></a
-					>{:else}{source.name}{/if}{#if index !== action.sources.length - 1}<span>, </span>{/if}
-			{/each}
-		</b>
-	</p>
+	{#if action.sources.length}
+		<p class="action-sources {textClasses}">
+			<b>
+				{#each action.sources as source, index}
+					{#if source.link}<a href={source.link} rel="external" target="_blank"
+							>{source.name}<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+								><path
+									d="m14.828 12 1.415 1.414 2.828-2.828a4 4 0 0 0-5.657-5.657l-2.828 2.828L12 9.172l2.828-2.829a2 2 0 1 1 2.829 2.829L14.828 12zM12 14.829l1.414 1.414-2.828 2.828a4 4 0 0 1-5.657-5.657l2.828-2.828L9.172 12l-2.829 2.829a2 2 0 1 0 2.829 2.828L12 14.828z"
+									fill="currentColor"
+								/><path
+									d="M14.829 10.586a1 1 0 0 0-1.415-1.415l-4.242 4.243a1 1 0 1 0 1.414 1.414l4.242-4.242z"
+									fill="currentColor"
+								/></svg
+							></a
+						>{:else}{source.name}{/if}{#if index !== action.sources.length - 1}<span>, </span>{/if}
+				{/each}
+			</b>
+		</p>
+	{/if}
 
 	<p class="action-subcategory">
 		<span class="tag is-small"><b>{action.subcategory.name}</b></span>
@@ -56,7 +58,7 @@
 		}
 
 		.action-sources {
-			margin: 0.5rem 0;
+			margin-top: 0.5rem;
 
 			a {
 				color: inherit;
@@ -71,6 +73,7 @@
 
 		.action-subcategory {
 			display: flex; // Corrige un problème de taille
+			margin-top: 0.5rem;
 		}
 	}
 </style>
