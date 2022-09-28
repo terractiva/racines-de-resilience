2b182368e578b1444d8bf9d32036630363cccd36
Thomas Seignette <tseignette@pm.me>
2022-09-28 14:05:09 +0200

    fonctionnalité: Ajout des actualités

--- site/templates/proof-of-concept.php
+++ site/templates/proof-of-concept.php
@@ -1,7 +1,33 @@
 <?php snippet('header', ['example' => 'proof-of-concept']) ?>
 
 <main class="main">
-  <p>TODO:</p>
+  <section>
+    <h2 class="h2">Actualités</h2>
+
+    <ul class="grid" style="--columns: 4">
+      <?php foreach ($page->news()->toStructure() as $newsItem): ?>
+      <li class="column">
+        <h3 class="h3 text"><?= $newsItem->title()->text() ?></h3>
+        <div class="text">
+          <?= $newsItem->content()->content()->sanitize() ?>
+        </div>
+        <?php
+          $buttonAttr = ['class' => 'button'];
+
+          if ($newsItem->buttonType() == 'url'):
+        ?>
+        <a href="<?= $newsItem->buttonUrl() ?>">
+          <?= $newsItem->buttonLabel()->text() ?>
+        </a>
+        <?php elseif ($newsItem->buttonType() == 'email'): ?>
+        <a href="mailto:<?= Str::encode($newsItem->buttonEmail()) ?>">
+          <?= $newsItem->buttonLabel()->text() ?>
+        </a>
+        <?php endif ?>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </section>
 </main>
 
 <?php snippet('footer') ?>
