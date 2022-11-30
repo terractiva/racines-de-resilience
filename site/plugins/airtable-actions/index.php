9b934bd733947e4c359629dcc58cb8c5a1bc9a80
Thomas Seignette <tseignette@pm.me>
2022-11-30 15:42:31 +0100

    correction: Déduplication des actions corrigée

--- site/plugins/airtable-actions/index.php
+++ site/plugins/airtable-actions/index.php
@@ -1,6 +1,64 @@
 <?php
 
-function fetchActions($base, $table, $view, $offset = null) {
+function actionsToPages($cacheName, $actions) {
+  $kirby = kirby();
+  $cache = $kirby->cache('airtable');
+
+  if ($kirby->request()->query()->get('flush-cache') !== null) $cache->remove($cacheName);
+
+  $pages = $cache->get($cacheName);
+
+  if ($pages === null) {
+    $pages = [];
+
+    foreach ($actions as $action) {
+      $name = $action['name'];
+      $pageIndex = array_search($name, array_column($pages, 'name'));
+
+      if ($pageIndex === false) {
+        $pages[] = [
+          'model' => 'action',
+          'name' => $name,
+          'slug' => Str::slug($name),
+          'content' => [
+            'categories' => $action['categories'],
+            'level' => $action['level'],
+            'position' => $action['position'] ?? null,
+            'positionId' => $action['positionId'] ?? null,
+            'radius' => $action['radius'] ?? null,
+            'sources' => [],
+            'subcategory' => findSubcategory($action['subcategoryId']),
+            'title' => $name
+          ]
+        ];
+        $pageIndex = count($pages) - 1;
+      }
+      if ($action['source']['name']) $pages[$pageIndex]['content']['sources'][] = $action['source'];
+    }
+
+    $cache->set($cacheName, $pages, 24 * 60); // 1 journée
+  }
+
+  return $pages;
+}
+
+function fetchRecords($cacheName, $base, $table, $view) {
+  $kirby = kirby();
+  $cache = $kirby->cache('airtable');
+
+  if ($kirby->request()->query()->get('flush-cache') !== null) $cache->remove($cacheName);
+
+  $records = $cache->get($cacheName);
+
+  if ($records === null) {
+    $records = fetchRecordsRecursive($base, $table, $view);
+
+    $cache->set($cacheName, $records, 24 * 60); // 1 journée
+  }
+
+  return $records;
+}
+function fetchRecordsRecursive($base, $table, $view, $offset = null) {
   $response = Remote::get(
     'https://api.airtable.com/v0/' . $base . '/' . $table . '?view=' . $view . ($offset === null ? '' : '&offset=' . $offset),
     ['headers' => ['Authorization: Bearer ' . env('AIRTABLE_KEY')]]
@@ -10,7 +68,7 @@ function fetchActions($base, $table, $view, $offset = null) {
     $responseJson = $response->json();
 
     return isset($responseJson['offset'])
-      ? array_merge($responseJson['records'], fetchActions($base, $table, $view, $responseJson['offset']))
+      ? array_merge($responseJson['records'], fetchRecordsRecursive($base, $table, $view, $responseJson['offset']))
       : $responseJson['records'];
   }
 
@@ -27,43 +85,26 @@ class ActionsPage extends Page
 {
   public function actions()
   {
-    $kirby = kirby();
-    $cache = $kirby->cache('airtable');
-    $cacheName = 'actions-' . $this->airtableView();
-    if ($kirby->request()->query()->get('flush-cache') !== null) $cache->remove($cacheName);
-    $actions = $cache->get($cacheName);
+    $actions = [];
+    $records = fetchRecords(
+      'actions-' . $this->airtableView(),
+      $this->airtableBase(),
+      $this->airtableTable(),
+      $this->airtableView()
+    );
 
-    if ($actions === null) {
-      $actions = [];
-      $records = fetchActions($this->airtableBase(), $this->airtableTable(), $this->airtableView());
-
-      foreach ($records as $record) {
-        $name = $record['fields']['Name'] ?? '';
-        $pageIndex = array_search($name, array_column($actions, 'name'));
-
-        if ($pageIndex === false) {
-          $actions[] = [
-            'model' => 'action',
-            'name' => $name,
-            'slug' => Str::slug($name),
-            'content' => [
-              'categories' => $record['fields']['Category - ID'][0] ?? '',
-              'level' => $record['fields']['Level - ID'][0] ?? null,
-              'sources' => [],
-              'subcategory' => findSubcategory($record['fields']['Theme - ID'][0] ?? ''),
-              'title' => $name
-            ]
-          ];
-          $pageIndex = count($actions) - 1;
-        }
-
-        $actions[$pageIndex]['content']['sources'][] = [
+    foreach ($records as $record) {
+      $categories = $record['fields']['Categories - ID'] ?? [];
+      $actions[] = [
+        'categories' => join(', ', $categories),
+        'level' => $record['fields']['Level - ID'][0] ?? null,
+        'name' => $record['fields']['Name'] ?? '',
+        'source' => [
           'name' => $record['fields']['Source - Name'] ?? '',
           'link' => $record['fields']['Source - Link'] ?? null
-        ];
-      }
-
-      $cache->set($cacheName, $actions, 24 * 60); // 1 journée
+        ],
+        'subcategoryId' => $record['fields']['Theme - ID'][0] ?? ''
+      ];
     }
 
     return $actions;
@@ -71,9 +112,11 @@ class ActionsPage extends Page
 
   public function children()
   {
-    $pages = $this->actions();
+    $actions = $this->actions();
 
-    foreach (collection('trees') as $tree) $pages = array_merge($pages, $tree->actions());
+    foreach (collection('trees') as $tree) $actions = array_merge($actions, $tree->actions());
+
+    $pages = actionsToPages('pages', $actions);
 
     return Pages::factory($pages, $this);
   }
@@ -83,65 +126,45 @@ class TreePage extends Page
 {
   public function actions()
   {
-    $kirby = kirby();
-    $cache = $kirby->cache('airtable');
-    $cacheName = 'tree-actions-' . $this->airtableView();
-    if ($kirby->request()->query()->get('flush-cache') !== null) $cache->remove($cacheName);
-    $actions = $cache->get($cacheName);
+    $actions = [];
+    $records = fetchRecords(
+      'tree-actions-' . $this->airtableView(),
+      $this->airtableBase(),
+      $this->airtableTable(),
+      $this->airtableView()
+    );
+    $positions = json_decode(file_get_contents(__DIR__ . '/positions.json'));
+    $positionIds = array_column($positions, 'id');
 
-    if ($actions === null) {
-      $actions = [];
-      $records = fetchActions($this->airtableBase(), $this->airtableTable(), $this->airtableView());
-      $positions = json_decode(file_get_contents('site/plugins/airtable-actions/positions.json'));
-      $positionIds = array_column($positions, 'id');
+    foreach ($records as $record) {
+      $positionIndex = array_search($record['fields']['ID'] ?? null, $positionIds);
 
-      foreach ($records as $record) {
-        $recordId = $record['fields']['ID'] ?? null;
-        $existingActionIndex = array_search($recordId, array_column($actions, 'positionId'));
-        $actionIndex = $existingActionIndex;
-
-        if ($existingActionIndex === false) {
-          $positionIndex = array_search($recordId, $positionIds);
-
-          if ($positionIndex !== false) {
-            $name = $record['fields']['Name'] ?? '';
-            $position = $positions[$positionIndex];
-            $actions[] = [
-              'model' => 'action',
-              'positionId' => $position->id,
-              'slug' => Str::slug($name),
-              'content' => [
-                'categories' => $position->categories,
-                'level' => $position->level,
-                'position' => $position->position,
-                'positionId' => $position->id,
-                'radius' => $position->radius,
-                'sources' => [],
-                'subcategory' => findSubcategory($position->subcategory),
-                'title' => $name
-              ]
-            ];
-            $actionIndex = count($actions) - 1;
-          }
-        }
-
-        if ($actionIndex !== false) {
-          $actions[$actionIndex]['content']['sources'][] = [
+      if ($positionIndex !== false) {
+        $position = $positions[$positionIndex];
+        $actions[] = [
+          'categories' => $position->categories,
+          'level' => $position->level,
+          'name' => $record['fields']['Name'] ?? '',
+          'position' => $position->position,
+          'positionId' => $position->id,
+          'radius' => $position->radius,
+          'source' => [
             'name' => $record['fields']['Source - Name'] ?? '',
             'link' => $record['fields']['Source - Link'] ?? null
-          ];
-        }
+          ],
+          'subcategoryId' => $position->subcategory
+        ];
       }
-
-      $cache->set($cacheName, $actions, 24 * 60); // 1 journée
-    };
+    }
 
     return $actions;
   }
 
   public function children()
   {
-    return Pages::factory($this->actions(), $this);
+    $pages = actionsToPages('tree-pages-' . $this->slug(), $this->actions());
+
+    return Pages::factory($pages, $this);
   }
 }
 
